84d3019b2ac3b9ac24fc631d3a3aaf04cfed1951
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2022-07-29 19:05:46 +0300

    Not Ortalaması Update

--- Not_Ortalaması_Hesaplama/NotOrtalamasiHesaplama/src/app.java
+++ Not_Ortalaması_Hesaplama/NotOrtalamasiHesaplama/src/app.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
 public class app {
     public static void main(String[] args) {
-        
+        int mat, fizik, kimya, turkce, tarih, muzik;
+
+        Scanner inp = new Scanner(System.in);
+
+        System.out.println("Matematik Notunuz: ");
+        mat = inp.nextInt();
+
+        System.out.println("Fizik Notunuz: ");
+        fizik = inp.nextInt();
+
+        System.out.println("Kimya Notunuz: ");
+        kimya = inp.nextInt();
+
+        System.out.println("Turkce Notunuz: ");
+        turkce = inp.nextInt();
+
+        System.out.println("Tarih Notunuz: ");
+        tarih = inp.nextInt();
+
+        System.out.println("Muzik Notunuz: ");
+        muzik = inp.nextInt();
+
+        int toplam = (mat+fizik+kimya+turkce+tarih+muzik);
+        double sonuc = toplam/6.0;
+
+        System.out.println("Ortalamanız: " + sonuc);
+
+        String check = sonuc >= 60 ? "Sınıfı Geçti :)" : "Sınıfta Kaldı!";
+        System.out.println(check);
+
+
     }
 }
