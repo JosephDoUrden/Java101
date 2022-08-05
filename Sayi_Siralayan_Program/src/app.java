4404da7da28eb0aaf07812955719f54cf8929296
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2022-08-05 11:56:16 +0300

    Sayı Sıralama

---
+++ Sayi_Siralayan_Program/src/app.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class app {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int a, b, c;
+
+        System.out.print("1. Sayı: ");
+        a = input.nextInt();
+
+        System.out.print("2. Sayı: ");
+        b = input.nextInt();
+
+        System.out.print("3. Sayı: ");
+        c = input.nextInt();
+
+        System.out.print("Büyükten küçüğe sıralama: ");
+        if((a > b) && (a > c)){
+            if(b > c){
+                System.out.println("a > b > c");
+            }
+            else{
+                System.out.println("a > c > b");
+            }
+        } else if ((b > a) && (b > c)) {
+            if(a > c){
+                System.out.println("b > a > c");
+            }
+            else {
+                System.out.println("b > c > a");
+            }
+        }else {
+            if(a > b){
+                System.out.println("c > a > b");
+            }else {
+                System.out.println("c > b > a");
+            }
+        }
+
+        System.out.print("Küçükten büyüğe sıralama: ");
+        if((a < b) && (a < c)){
+            if(b < c){
+                System.out.println("a < b < c");
+            }else {
+                System.out.println("a < c < b");
+            }
+        } else if ((b < a) && (b < c)) {
+            if(a < c){
+                System.out.println("b < a < c");
+            }else {
+                System.out.println("b < c < a");
+            }
+        }else {
+            if(a < b){
+                System.out.println("c < a < b");
+            }else {
+                System.out.println("c < b < a");
+            }
+        }
+
+    }
+}
