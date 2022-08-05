5f5c2fb343b96fd94df000b6c4b21d0d4b79aef4
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2022-08-05 18:57:58 +0300

    zodiac

---
+++ Zodiac/src/app.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class app {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int year, zodiac;
+
+        System.out.print("Doğum yılınız: ");
+        year = input.nextInt();
+
+        zodiac = year % 12;
+
+        switch (zodiac){
+            case 0:
+                System.out.println("Maymun");
+                break;
+            case 1:
+                System.out.println("Horoz");
+                break;
+            case 2:
+                System.out.println("Köpek");
+                break;
+            case 3:
+                System.out.println("Domuz");
+                break;
+            case 4:
+                System.out.println("Fare");
+                break;
+            case 5:
+                System.out.println("Öküz");
+                break;
+            case 6:
+                System.out.println("Kaplan");
+                break;
+            case 7:
+                System.out.println("Tavşan");
+                break;
+            case 8:
+                System.out.println("Ejderha");
+                break;
+            case 9:
+                System.out.println("Yılan");
+                break;
+            case 10:
+                System.out.println("At");
+                break;
+            case 11:
+                System.out.println("Koyun");
+                break;
+            default:
+                System.out.println("Burcunuz bulunamadı!");
+                break;
+        }
+
+    }
+}
