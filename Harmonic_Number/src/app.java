777db4150768d1b97b501800b55f169e42e7342c
JosephDoUrden <yusufhan.sacak@bahcesehir.edu.tr>
2022-08-10 09:27:08 +0300

    Harmonic Number

---
+++ Harmonic_Number/src/app.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class app {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("N sayısını giriniz: ");
+        int n = input.nextInt();
+
+        double result = 0;
+
+        for (double i = 1; i <= n; i++){
+            result += (1/i);
+        }
+
+        System.out.println(result);
+    }
+}
